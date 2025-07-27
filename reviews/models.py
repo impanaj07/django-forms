4611272fbffc5071166b01c8a6dae3b9bf218b23
impanajgowda14@gmail.com <154488357+impanaj07@users.noreply.github.com>
2025-07-27 15:27:42 +0530

    added models

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+class Review(models.Model):
+    user_name = models.CharField(max_length=100)
+    review_text = models.TextField()
+    rating = models.IntegerField()
 
+    def __str__(self):
+        return f"{self.user_name} - {self.rating}/5"
 # Create your models here.
