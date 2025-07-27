c5dba9cffc366f29309bc91fbbd09167035c8fee
impanajgowda14@gmail.com <154488357+impanaj07@users.noreply.github.com>
2025-07-27 15:11:35 +0530

    added styling

--- reviews/urls.py
+++ reviews/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views  
 urlpatterns = [
  path("",views.review),
-    path("thank_you/",views.thank_you),
+ path("thank_you/",views.thank_you),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -8,12 +8,11 @@ def review(request):
         form=ReviewForm(request.POST)
         if form.is_valid():
             user_name = form.cleaned_data['user_name']
-            review_text = form.cleaned_data['review']
-            entered_text = f"Name: {user_name}, Review: {review_text}"
-
-        print(entered_text)
-        return HttpResponseRedirect('/thank_you/')
-    form=ReviewForm()
+            entered_text = f"Name: {user_name}"
+            print(entered_text)
+            return HttpResponseRedirect('/thank_you/')
+    else:
+        form=ReviewForm()
     return render(request,"reviews/review.html", {'form': form} )
 def thank_you(request):
     return render(request,"reviews/thank_you.html")
